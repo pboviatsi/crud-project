029fc7e0e91e3d34bb0763de70d4a28648d1ddd0
pbovc <pboviatsi@knowledge.gr>
2020-08-11 16:20:21 +0300

    change the table to material-table

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,31 +1,30 @@
 import React from 'react';
-import './App.css';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Container from "@material-ui/core/Container/Container";
+import AppBar from "@material-ui/core/AppBar/AppBar";
+import Toolbar from "@material-ui/core/Toolbar/Toolbar";
+import Typography from "@material-ui/core/Typography/Typography";
 import Products from "./components/Products";
-import Typography from "@material-ui/core/Typography";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Container from "@material-ui/core/Container";
-import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
+
 
 function App() {
-    return (
-         <Container maxWidth="xl" >
+    return(
+        <Container maxWidth="xl">
             <AppBar position="static">
                 <Toolbar>
                     <Typography variant="h5" component="h1" gutterBottom>
                         Διαχείριση προϊόντων
                     </Typography>
-                </Toolbar><div>
-
-            </div>
+                </Toolbar>
             </AppBar>
-             <Router>
-                 <Switch>
-                     <Route path="/" component={Products} exact />
-                 </Switch>
-             </Router>
+
+            <Router>
+                <Switch>
+                    <Route path="/" component={Products} exact/>
+                </Switch>
+            </Router>
         </Container>
-    );
+    )
 }
 
 export default App;
--- Client/src/components/Products.js
+++ Client/src/components/Products.js
@@ -1,27 +1,47 @@
 import React, {useEffect, useState} from "react";
+import MaterialTable from 'material-table'
 import axios from "axios";
+import { forwardRef } from 'react';
 
-import {makeStyles} from "@material-ui/core";
-import Table from "@material-ui/core/Table/Table";
-import TableContainer from "@material-ui/core/TableContainer/TableContainer";
-import TableHead from "@material-ui/core/TableHead/TableHead";
-import TableRow from "@material-ui/core/TableRow/TableRow";
-import TableBody from "@material-ui/core/TableBody/TableBody";
-import TableCell from "@material-ui/core/TableCell/TableCell";
-import Paper from "@material-ui/core/Paper/Paper";
-import Button from "@material-ui/core/Button/Button";
-import { Link } from "react-router-dom";
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
 
-const useStyles = makeStyles({
-    table: {
-        //minWidth: 650,
-    },
-});
+const tableIcons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+};
 
-export default function Products(props) {
-    const classes = useStyles();
+export default function Product(props) {
+
     const [rows, setRows] = useState([]);
-
     useEffect(() => {
         const fetchData = async () => {
             const result = await axios(
@@ -32,40 +52,116 @@ export default function Products(props) {
 
         fetchData();
     }, []);
+    const [columns, setColumns] = useState([
+        {   title: 'Κωδικός',
+            field: 'product_id',
+            headerStyle: {
+                backgroundColor: '#d3dfe5',
+                textAlign: "center"
+            }
+        },
+        {
+            title: 'Τίτλος',
+            field: 'name_product',
+            cellStyle: {
+                textAlign: "left"
+            },
+            headerStyle: {
+                backgroundColor: '#d3dfe5',
+                textAlign: "center"
+            }
+        },
+        {   title: 'Τιμή',
+            field: 'price',
+            type: 'numeric',
+            cellStyle: {
+                textAlign: "center"
+            },
+            headerStyle: {
+                backgroundColor: '#d3dfe5',
+                textAlign: "center"
+            }
+        },
+        {   title: 'Διαθεσιμότητα',
+            field: 'availability_count',
+            type: 'numeric',
+            cellStyle: {
+                textAlign: "center"
+            },
+            headerStyle: {
+                backgroundColor: '#d3dfe5',
+                textAlign: "center"
+            }
+        },
+        {   title: 'Url προϊόντος',
+            field: 'product_link',
+            cellStyle: {
+                textAlign: "left"
+            },
+            headerStyle: {
+                backgroundColor: '#d3dfe5',
+                textAlign: "center"
+            }
+        },
+    ]);
 
     return (
-        <>
-            <TableContainer component={Paper}>
-                <Table className={classes.table} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Κωδικός</TableCell>
-                            <TableCell align="center">Τίτλος</TableCell>
-                            <TableCell align="center">Περιγραφή</TableCell>
-                            <TableCell align="center">Τιμή</TableCell>
-                            <TableCell align="center">Διαθεσιμότητα</TableCell>
-                            <TableCell align="center">Url προϊόντος</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows.map((row) => (
-                            <TableRow key={row.product_id}>
-                                <TableCell component="th" scope="row">
-                                    {row.product_id}
-                                </TableCell>
-                                <TableCell align="left">{row.name_product}</TableCell>
-                                <TableCell align="left">{row.descr}</TableCell>
-                                <TableCell align="center">{row.price}</TableCell>
-                                <TableCell align="center">{row.availability_count}</TableCell>
-                                <TableCell align="left">{row.product_link}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            <Button variant="contained" color="primary" component={Link}>
-                Νέο προϊόν
-            </Button>
-        </>
-    );
+        <MaterialTable
+            icons={tableIcons}
+            title="Διαχείριση προϊόντων"
+            columns={columns}
+            data={rows}
+            editable={{
+                onRowAdd: newData =>
+                    new Promise((resolve, reject) => {
+                        setTimeout(() => {
+                            setRows([...rows, newData]);
+
+                            resolve();
+                        }, 1000)
+                    }),
+                onRowUpdate: (newData, oldData) =>
+                    new Promise((resolve, reject) => {
+                        setTimeout(() => {
+                            const dataUpdate = [...rows];
+                            const index = oldData.tableData.id;
+                            dataUpdate[index] = newData;
+                            setRows([...dataUpdate]);
+
+                            resolve();
+                        }, 1000)
+                    }),
+                onRowDelete: oldData =>
+                    new Promise((resolve, reject) => {
+                        setTimeout(() => {
+                            const dataDelete = [...rows];
+                            const index = oldData.tableData.id;
+                            dataDelete.splice(index, 1);
+                            setRows([...dataDelete]);
+
+                            resolve()
+                        }, 1000)
+                    }),
+            }}
+            detailPanel={[
+                {
+                    tooltip: 'Δείτε περισσότερα',
+                    render: rowData => {
+                        return (
+                            <div
+                                style={{
+                                    fontSize: 15,
+                                    textAlign: 'left',
+                                    backgroundColor: '#dee2ff',
+                                    padding: '40px',
+                                }}
+                            >
+                                <b> Περιγραφή προϊόντος:</b> {rowData.descr}
+                            </div>
+                        )
+                    },
+                },
+            ]}
+        />
+    )
 }
