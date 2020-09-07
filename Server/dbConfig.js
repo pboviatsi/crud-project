6f33da23901357ad778e7f0f5cc747143bcfa0df
pbovc <pboviatsi@knowledge.gr>
2020-09-07 16:23:59 +0300

    heroku new generic package.json

--- Server/dbConfig.js
+++ Server/dbConfig.js
@@ -5,7 +5,8 @@ const pool = new Pool({
     host: 'ec2-54-75-150-32.eu-west-1.compute.amazonaws.com',
     database: 'd6cvoliqhemaap',
     password: 'a34cf770ac97ee8609800b95596c06902db25fa3b65bc9cfcbbba8099c1e9358',
-    port: 5432
+    port: 5432,
+    ssl:true
 });
 
 module.exports = pool;
