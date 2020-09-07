8c21584a27292ae8c3b9e8e5127bfca1680df280
pbovc <pboviatsi@knowledge.gr>
2020-09-07 14:26:22 +0300

    heroku conf

--- Server/dbConfig.js
+++ Server/dbConfig.js
@@ -1,10 +1,10 @@
 //στοιχεία σύνδεσης της βάση δεδομένων
 const Pool = require('pg').Pool;
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'crud-project',
-    password: 'penny@bove',
+    user: 'qyiqgpykypjvva',
+    host: 'ec2-54-75-150-32.eu-west-1.compute.amazonaws.com',
+    database: 'd6cvoliqhemaap',
+    password: 'a34cf770ac97ee8609800b95596c06902db25fa3b65bc9cfcbbba8099c1e9358',
     port: 5432
 });
 
