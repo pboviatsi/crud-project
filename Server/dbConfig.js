cc8f267a48e13fea9ed46f51c46c74e5e3e659c2
pbovc <pboviatsi@knowledge.gr>
2020-09-08 15:37:23 +0300

    heroku settings

--- Server/dbConfig.js
+++ Server/dbConfig.js
@@ -1,11 +1,7 @@
 //στοιχεία σύνδεσης της βάση δεδομένων
 const Pool = require('pg').Pool;
 const pool = new Pool({
-    user: 'qyiqgpykypjvva',
-    host: 'ec2-54-75-150-32.eu-west-1.compute.amazonaws.com',
-    database: 'd6cvoliqhemaap',
-    password: 'a34cf770ac97ee8609800b95596c06902db25fa3b65bc9cfcbbba8099c1e9358',
-    port: 5432,
+    connectionString:'postgres://qyiqgpykypjvva:a34cf770ac97ee8609800b95596c06902db25fa3b65bc9cfcbbba8099c1e9358@ec2-54-75-150-32.eu-west-1.compute.amazonaws.com:5432/d6cvoliqhemaap'
 });
 
 module.exports = pool;
